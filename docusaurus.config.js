049ea86a6af4f6ea2f0a57d338a088de105b7d39
Amarachi Iheanacho <amarachiiheanacho@Amarachis-MacBook-Pro.local>
2024-05-05 15:44:42 +0100

    added this

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -43,8 +43,8 @@ const config = {
           sidebarPath: './sidebars.js',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/Iheanacho-ai/Eyer-documentation/tree/main/docs',
+          // editUrl:
+          //   'https://github.com/Iheanacho-ai/Eyer-documentation/tree/main/docs',
         },
         blog: false,
         //{
